b03751d2d81d38016f0c5a28ce5bb3c06857db61
Patrick <whitep@vcu.edu>
2014-06-11 10:58:41 -0400

    getting closer

--- scpeace.rb
+++ scpeace.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 require 'net/scp'
-
 # Upload local file/directory to remote directory
-file_name = ARGV
-if file_name != nil
-  Net::SCP.upload!("192.168.2.3", "patrick",
-  file_name, "/home/patrick/media/movies",
-  :ssh => { :password => "b1gPimpin"})
-else
-  puts "Specify a file turkey"
+
+uFile = ARGV[0]
+nFile = ARGV[1]
+
+File.open(uFile) do |f|
+  Net::SCP.start("192.168.2.3", "patrick", :password => "b1gPimpin") do |scp|
+  scp.upload! uFile, "/home/patrick/media/movies/" + nFile
+  end
 end
---
+++ test.rb
@@ -0,0 +1,7 @@
+file_name = ARGV
+
+if file_name == nil
+  puts "AINT NONE"
+else
+  puts "File exists"
+end
