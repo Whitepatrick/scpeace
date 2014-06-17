ae2b9497bc69ff776d3d1a6e0179e22e07a0ce75
Patrick <whitep@vcu.edu>
2014-06-17 12:36:49 -0400

    Version 0.0.1

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ scpeace
 =======
 
 Ruby script to move Movie/TV files from local to remote
+-----------------------
+
+
+Contents:
+README.md
+mediaMove.rb
+-----------------------
+
+
+Requires net/scp gem
+
+gem install net-scp
+-----------------------
+
+Edit methods within script with your host, user name,
+password, and remote file path.
+
+ruby mediaMove.rb <file/dir> <new name>
+-----------------------
--- mediaMove.rb
+++ mediaMove.rb
@@ -1,43 +1,46 @@
 #!/usr/bin/env ruby
 require 'net/scp'
 # Upload local file/directory to remote directory
+# ruby mediaMove.rb <file/folder> <new name>
 
-# accepts two arguments 
+# Requires two arguments 
 # mediaObject = file/folder to move from local to remote
 # newName = new name of file/folder on remote file system (keeps names within dir)
 
 mediaObject = ARGV[0]
 newName = ARGV[1]
 
-# Main logic for where to send media object
-if mediaObject == nil
-  puts "Specify a file or folder, Turkey!"
-elsif
-  Dir.exist?(mediaObject)
-  DirUp(mediaObject, newName)
-  puts "Directory Succeffuly Uploaded!"
-elsif
-  File.exists?(uploadFile)
-  FileUp(mediaObject, newName)
-  puts "File Successfully Uploaded!"
-end
-
 # Method for moving single file from local to remote
-def FileUp(mediaObject, newName)
+# Edit credentials for remote host
+def fileUp(mediaObject, newName)
   File.open(mediaObject) do |f|
-    Net::SCP.start("192.168.2.3", "patrick", :password => "password") do |scp|
-    scp.upload mediaObject, "/home/patrick/media/movies/" + newName,
+    Net::SCP.start("remote host", "user", :password => "password") do |scp|
+    scp.upload mediaObject, "remote file path" + newName,
     :verbose => true
     end
   end
 end
 
 # Method for recursively moving a directory from local to remote
-def DirUp(mediaObject)
-  Dir.open(mediObject) do |f|
-    Net::SCP.start("192.168.2.3", "patrick", :password => "password") do |scp|
-    scp.upload mediaObject, "/home/patrick/media/movies/" + newName,
+# Edit credentials for remote host
+def dirUp(mediaObject, newName)
+  Dir.open(mediaObject) do |f|
+    Net::SCP.start("remote host", "user", :password => "password") do |scp|
+    scp.upload mediaObject, "remote file path" + newName,
     :recursive => true, :verbose => true
     end
   end
+end
+
+# Main logic for where to send media object
+if mediaObject == nil
+  puts "Specify a file or folder, Turkey!"
+elsif
+  Dir.exists?(mediaObject)
+  dirUp(mediaObject, newName)
+  puts "Directory Succeffuly Uploaded!"
+elsif
+  File.exists?(mediaObject)
+  fileUp(mediaObject, newName)
+  puts "File Successfully Uploaded!"
 end
