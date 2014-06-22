77cf3ef183ed86256efb02e8b8f201ead088c16b
Patrick <whitep@vcu.edu>
2014-06-21 20:24:20 -0400

    Working on destination function

--- README.md
+++ README.md
@@ -6,18 +6,17 @@ Ruby script to move Movie/TV files from local to remote
 
 
 Contents:
+-----------------------
 README.md
 mediaMove.rb
------------------------
-
 
 Requires net/scp gem
-
-gem install net-scp
 -----------------------
+gem install net-scp
 
+Use
+-----------------------
 Edit methods within script with your host, user name,
 password, and remote file path.
 
 ruby mediaMove.rb <file/dir> <new name>
------------------------
--- mediaMove.rb
+++ mediaMove.rb
@@ -1,21 +1,29 @@
 #!/usr/bin/env ruby
 require 'net/scp'
+
 # Upload local file/directory to remote directory
-# ruby mediaMove.rb <file/folder> <new name>
+# ruby mediaMove.rb <file/folder> <new name> <tv/movies>
 
 # Requires two arguments 
 # mediaObject = file/folder to move from local to remote
 # newName = new name of file/folder on remote file system (keeps names within dir)
+# destination = switch between two paths (tv or movies in my case)
 
 mediaObject = ARGV[0]
 newName = ARGV[1]
+destination = ARGV[2]
+
+# Add vars here
+# Need vars to be passed to methods
+
+
 
 # Method for moving single file from local to remote
 # Edit credentials for remote host
 def fileUp(mediaObject, newName)
   File.open(mediaObject) do |f|
-    Net::SCP.start("remote host", "user", :password => "password") do |scp|
-    scp.upload mediaObject, "remote file path" + newName,
+    Net::SCP.start(remoteServer, login, :password => password) do |scp|
+    scp.upload mediaObject, remotePath + newName,
     :verbose => true
     end
   end
@@ -25,14 +33,26 @@ end
 # Edit credentials for remote host
 def dirUp(mediaObject, newName)
   Dir.open(mediaObject) do |f|
-    Net::SCP.start("remote host", "user", :password => "password") do |scp|
-    scp.upload mediaObject, "remote file path" + newName,
+    Net::SCP.start(remoteServer, login, :password => password) do |scp|
+    scp.upload mediaObject, remotePath + newName,
     :recursive => true, :verbose => true
     end
   end
 end
 
-# Main logic for where to send media object
+# Pre sort media objects destination
+if destination != 'tv' || 'movies'
+  puts 'Error: specify destination (movies/tv)'
+elsif
+  destination == 'tv'
+  remotePath = tvPath
+  puts 'Transferring to TV'
+else
+  remotePath = moviesPath
+  puts 'Transferring to Movies'
+end
+
+# Main logic on how to send media object
 if mediaObject == nil
   puts "Specify a file or folder, Turkey!"
 elsif
